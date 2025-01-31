af7aa7c8949d3792479a02456b0cb84ff22a66c5
akshit_wadhwa <akshitwadhwa06@gmail.com>
2025-01-31 19:36:31 +0530

    Update and rename solution approach twosome.py to solution approach two sum.py

---
+++ solution approach two sum.py
@@ -0,0 +1,9 @@
+In this question we were given a test case arraya and we had to find the numner in the array whoes sum resulted as the given number 
+for ex - 
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+
+Now here we have 2 and 7 as the given number whose sum will give you the number 9.
+So I have used the  brute force approach here where we take the first element which is 2 and add it with the other elements in the given array
+so After that the algo will run till n-1 where it will check until it gets the number sum which is 9
